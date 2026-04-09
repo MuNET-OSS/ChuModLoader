58f913400b57bb627d8aef84dd51fe6ac51b5654
Applesaber <aotu013@163.com>
2026-04-09 18:55:11 +0800

    chore: 用forward_dll替换naked asm导出转发

--- build.rs
+++ build.rs
@@ -1,9 +1,5 @@
 fn main() {
-    println!(
-        "cargo:rustc-cdylib-link-arg=/DEF:{}",
-        std::path::Path::new("src/version.def")
-            .canonicalize()
-            .unwrap()
-            .display()
-    );
+    let system_drive = std::env::var("SYSTEMDRIVE").unwrap_or("C:".to_string());
+    let version_path = format!("{}\\Windows\\System32\\version.dll", system_drive);
+    forward_dll::forward_dll(&version_path).unwrap();
 }
